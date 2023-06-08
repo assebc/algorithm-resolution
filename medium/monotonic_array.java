3c9e5369d9cd0f893c6e5eea432ac0cb0fdff6a5
assebc <claudioafbessa@gmail.com>
2023-06-08 13:00:30 +0100

    feat: check if array is monotonic

---
+++ medium/monotonic_array.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Program {
+  public static boolean isMonotonic(int[] array) {
+    if(array.length<=1) return true;
+    boolean isDecreasing = array[0]>array[array.length-1];
+    for(int i = 1;i<array.length;i++){
+     if(isDecreasing && array[i]>array[i-1]) return false;
+     if(!isDecreasing && array[i]<array[i-1]) return false;
+    }
+    return true;
+  }
+}
+
